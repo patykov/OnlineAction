d7bd8e2ed7cef025f4444bb4d7e8aff894a042b5
Patricia Kovaleski <kovaleski@poli.ufrj.br>
2019-09-17 19:34:05 +0000

    Improving video reading

--- datasets/video_record.py
+++ datasets/video_record.py
@@ -6,28 +6,31 @@ class VideoRecord(object):
     def __init__(self, video_path, label, reliable=False):
         self.path = video_path
         self.label = label
-        self.video = cv2.VideoCapture(self.path)
-        self.num_frames = self._get_num_frames(reliable)
-        self.fps = self.video.get(cv2.CAP_PROP_FPS)
+        self.num_frames, self.fps = self._get_video_data(reliable)
 
-    def _get_num_frames(self, reliable):
-        success, frame = self.video.read()
+    def _get_video(self):
+        return cv2.VideoCapture(self.path)
+
+    def _get_video_data(self, reliable):
+        video = self._get_video()
+        success, _ = video.read()
         if not success:
             raise ValueError('Failed to load video {}'.format(self.path))
 
+        fps = video.get(cv2.CAP_PROP_FPS)
         if reliable:
             # Fastest and easiest way to get video frame count. However, it does not work for
             # Kinetics dataset, where most videos have a mismatch in the frame count.
-            count = self.video.get(cv2.CAP_PROP_FRAME_COUNT)
+            count = video.get(cv2.CAP_PROP_FRAME_COUNT)
         else:
             # Counting manually
             count = 0
             while(success):
-                success, frame = self.video.read()
+                success, _ = video.read()
                 count += 1
 
-        self.video.set(cv2.CAP_PROP_POS_AVI_RATIO, 0)  # Set video to the start
-        return count
+        video.release()
+        return count, fps
 
     def get_frames(self, indices):
         """
@@ -37,15 +40,16 @@ class VideoRecord(object):
             images : Dictionary in the format: {frame_id: PIL Image}
         """
         images = dict()
-        self.video.set(cv2.CAP_PROP_POS_FRAMES, min(indices))
-        for count in range(min(indices), max(indices)+1):
-            success, frame = self.video.read()
+        video = self._get_video()
+        for count in range(max(indices) + 1):
+            success, frame = video.read()
             if not success:
                 raise ValueError('Could not load frame {} from video {} (num_frames: {})\n'.format(
                     count, self.path, self.num_frames))
 
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             if count in indices:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 images[count] = Image.fromarray(frame)
 
+        video.release()
         return images
